99e1b5966672d55f30c6fb8d114f299bcb610d2e
John Shelby <johnshelby@yahoo.com>
2017-03-19 20:34:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-const app = "I don't do much."
+function bumpCounter() {
+  var counter = 0
+  function addBump() {
+    counter++
+    return counter
+  }
+  function getBumps() {
+    return counter
+  }
+
+  return {addBump,
+    getBumps};
+}
+
+function createAnimal(animalType) {
+    let innerFunction =function weaponized(deadlyDevice) {
+    return {"animalType":animalType, "deadlyDevice":deadlyDevice}
+  }
+  return innerFunction
+}
+
+
+let sharkCreator = createAnimal("Shark")
+
+let sharkWithFrickinLaserbeam = sharkCreator("Laserbeam")
+
+let sharkWithFrickinCannon = sharkCreator("Cannon")
